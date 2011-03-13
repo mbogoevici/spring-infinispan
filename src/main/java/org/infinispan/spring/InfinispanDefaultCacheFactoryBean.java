7ee3c57ca2b3f3dcbbf86fd276321e75b05e62b2
Olaf Bergner <olaf.bergner@gmx.de>
2011-03-13 14:01:37 +0100

    ISPN-961 - Implement a provider for Spring's Cache Abstraction
     * Create a new FactoryBean for creating default caches, i.e. caches
    that always use INFINISPAN's default configuration. This is a
    specialized version of the already existing FactoryBean for named
    caches.
     * Implement a test for this new FactoryBean.

---
+++ src/main/java/org/infinispan/spring/InfinispanDefaultCacheFactoryBean.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright (C) 2010.
+ * Olaf Bergner.
+ * Hamburg, Germany. olaf.bergner@gmx.de
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.infinispan.spring;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.infinispan.Cache;
+import org.infinispan.manager.CacheContainer;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * <p>
+ * A {@link org.springframework.beans.factory.FactoryBean <code>FactoryBean</code>} for creating a native
+ * <em>default</em> INFINISPAN {@link org.infinispan.Cache <code>org.infinispan.Cache</code>}, 
+ * delegating to a {@link #setInfinispanCacheContainer(CacheContainer) <code>configurable</code>} 
+ * {@link org.infinispan.manager.CacheContainer <code>org.infinispan.manager.CacheContainer</code>}. A
+ * default <code>Cache</code> is a <code>Cache</code> that uses its <code>CacheContainer</code>'s default
+ * settings. This is contrary to a <em>named</em> <code>Cache</code> where select settings from a
+ * <code>CacheContainer</code>'s default configuration may be overridden with settings specific to that
+ * <code>Cache</code>.
+ * </p>
+ * <p>
+ * In addition to creating a <code>Cache</code> this <code>FactoryBean</code> does also control that 
+ * <code>Cache</code>'s {@link org.infinispan.lifecycly.Lifecycle lifecycle} by shutting it down when the 
+ * enclosing Spring application context is closed. It is therefore advisable to <em>always</em> use this 
+ * <code>FactoryBean</code> when creating a <code>Cache</code>.
+ * </p>
+ *
+ * @author <a href="mailto:olaf.bergner@gmx.de">Olaf Bergner</a>
+ *
+ */
+public class InfinispanDefaultCacheFactoryBean implements FactoryBean<Cache<Object, Object>>, InitializingBean,
+		DisposableBean {
+
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	private CacheContainer infinispanCacheContainer;
+
+	private Cache<Object, Object> infinispanCache;
+
+	/**
+	 * <p>
+	 * Sets the {@link org.infinispan.manager.CacheContainer <code>org.infinispan.manager.CacheContainer</code>} to 
+	 * be used for creating our {@link org.infinispan.Cache <code>Cache</code>} instance. Note that this is a 
+	 * <strong>mandatory</strong> property.
+	 * </p>
+	 * 
+	 * @param infinispanCacheContainer 
+	 * 			The {@link org.infinispan.manager.CacheContainer <code>org.infinispan.manager.CacheContainer</code>} to
+	 *          be used for creating our {@link org.infinispan.Cache <code>Cache</code>} instance
+	 */
+	public void setInfinispanCacheContainer(final CacheContainer infinispanCacheContainer) {
+		this.infinispanCacheContainer = infinispanCacheContainer;
+	}
+
+	/**
+	 * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+	 */
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		if (this.infinispanCacheContainer == null) {
+			throw new IllegalStateException("No INFINISPAN CacheContainer has been set");
+		}
+		this.logger.info("Initializing named INFINISPAN cache ...");
+		this.infinispanCache = this.infinispanCacheContainer.getCache();
+		this.logger.info("New INFINISPAN cache [" + this.infinispanCache + "] initialized");
+	}
+
+	/**
+	 * @see org.springframework.beans.factory.FactoryBean#getObject()
+	 */
+	@Override
+	public Cache<Object, Object> getObject() throws Exception {
+		return this.infinispanCache;
+	}
+
+	/**
+	 * @see org.springframework.beans.factory.FactoryBean#getObjectType()
+	 */
+	@Override
+	public Class<? extends Cache> getObjectType() {
+		return this.infinispanCache != null ? this.infinispanCache.getClass() : Cache.class;
+	}
+
+	/**
+	 * Always returns <code>true</code>.
+	 * 
+	 * @return Always <code>true</code>
+	 * 
+	 * @see org.springframework.beans.factory.FactoryBean#isSingleton()
+	 */
+	@Override
+	public boolean isSingleton() {
+		return true;
+	}
+
+	/**
+	 * Shuts down the <code>org.infinispan.Cache</code> created by this <code>FactoryBean</code>.
+	 * 
+	 * @see org.springframework.beans.factory.DisposableBean#destroy()
+	 * @see org.infinispan.Cache#stop()
+	 */
+	@Override
+	public void destroy() throws Exception {
+		// Probably being paranoid here ...
+		if (this.infinispanCache != null) {
+			this.infinispanCache.stop();
+		}
+	}
+}
---
+++ src/test/java/org/infinispan/spring/InfinispanDefaultCacheFactoryBeanTest.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (C) 2010.
+ * Olaf Bergner.
+ * Hamburg, Germany. olaf.bergner@gmx.de
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.infinispan.spring;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.infinispan.Cache;
+import org.infinispan.lifecycle.ComponentStatus;
+import org.infinispan.manager.DefaultCacheManager;
+import org.junit.Test;
+
+/**
+ * <p>
+ * Test {@link InfinispanDefaultCacheFactoryBean}.
+ * </p>
+ *
+ * @author <a href="mailto:olaf.bergner@gmx.de">Olaf Bergner</a>
+ *
+ */
+public class InfinispanDefaultCacheFactoryBeanTest {
+
+	/**
+	 * Test method for {@link org.infinispan.spring.InfinispanDefaultCacheFactoryBean#getObject()}.
+	 * @throws Exception 
+	 */
+	@Test
+	public final void infinispanDefaultCacheFactoryBeanShouldProduceANonNullInfinispanCache() throws Exception {
+		final InfinispanDefaultCacheFactoryBean objectUnderTest = new InfinispanDefaultCacheFactoryBean();
+		objectUnderTest.setInfinispanCacheContainer(new DefaultCacheManager());
+		objectUnderTest.afterPropertiesSet();
+
+		final Cache<Object, Object> cache = objectUnderTest.getObject();
+
+		assertNotNull("InfinispanDefaultCacheFactoryBean should have produced a proper Infinispan cache. "
+				+ "However, it produced a null Infinispan cache.", cache);
+	}
+
+	/**
+	 * Test method for {@link org.infinispan.spring.InfinispanDefaultCacheFactoryBean#isSingleton()}.
+	 */
+	@Test
+	public final void infinispanDefaultCacheFactoryBeanShouldDeclareItselfToBeSingleton() {
+		final InfinispanDefaultCacheFactoryBean objectUnderTest = new InfinispanDefaultCacheFactoryBean();
+
+		assertTrue(
+				"InfinispanDefaultCacheFactoryBean should declare itself to produce a singleton. However, it didn't.",
+				objectUnderTest.isSingleton());
+	}
+
+	/**
+	 * Test method for {@link org.infinispan.spring.InfinispanDefaultCacheFactoryBean#destroy()}.
+	 * @throws Exception 
+	 */
+	@Test
+	public final void infinispanDefaultCacheFactoryBeanShouldStopTheCreatedInfinispanCacheWhenItIsDestroyed()
+			throws Exception {
+		final InfinispanDefaultCacheFactoryBean objectUnderTest = new InfinispanDefaultCacheFactoryBean();
+		objectUnderTest.setInfinispanCacheContainer(new DefaultCacheManager());
+		objectUnderTest.afterPropertiesSet();
+
+		final Cache<Object, Object> cache = objectUnderTest.getObject();
+		objectUnderTest.destroy();
+
+		assertEquals(
+				"InfinispanDefaultCacheFactoryBean should have stopped the created Infinispan cache when being destroyed. "
+						+ "However, the created Infinispan is not yet terminated.", ComponentStatus.TERMINATED,
+				cache.getStatus());
+	}
+}
